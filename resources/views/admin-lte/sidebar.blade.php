b72d779e52462dd4ad12fb16954e07a6483886d6
Angger's Macbook <myd82@192.168.1.12>
2023-03-14 17:39:42 +0700

    first commit

--- resources/views/admin-lte/sidebar.blade.php
+++ resources/views/admin-lte/sidebar.blade.php
@@ -1,8 +1,8 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">{{config('app.name')}}</span>
+      <img src="/adminlte/dist/img/logo-buku.png" alt="logo-buku" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">{{config('eLibrary', 'eLibrary')}}</span>
     </a>
 
     <!-- Sidebar -->
